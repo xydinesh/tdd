455e5844e819f6f6862ef78ee8ac7c6aaea5c844
Dinesh Weerapurage <xydinesh@gmail.com>
2020-12-26 21:48:19 -0700

    refactor multiplication test

--- multicurrency/multicurrency_test.go
+++ multicurrency/multicurrency_test.go
@@ -4,7 +4,6 @@ import "testing"
 
 /*
 * $5 + 10 CHF = $10 if rate is 2:1
-* Make amount private
 * Money rounding
 * equals()
 * hashCode()
@@ -15,11 +14,11 @@ import "testing"
 func TestMultiplication(t *testing.T) {
 	five := New(5)
 	prd := five.Times(2)
-	if prd.amount != 10 {
+	if !prd.Equals(New(10)) {
 		t.Errorf("Expected 10 but got %d", prd.amount)
 	}
 	prd = five.Times(3)
-	if prd.amount != 15 {
+	if !prd.Equals(New(15)) {
 		t.Errorf("Expected 15 but got %d", prd.amount)
 	}
 }
