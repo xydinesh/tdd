1528c4e9aef5d184179fac065c43bbc58703ef68
Dinesh Weerapurage <xydinesh@gmail.com>
2020-12-26 21:50:35 -0700

    adding comments for structure and methods

--- multicurrency/multicurrency.go
+++ multicurrency/multicurrency.go
@@ -1,19 +1,23 @@
 package multicurrency
 
+// Dollar structure for the book
 type Dollar struct {
 	amount int
 }
 
+// New method to create new Dollar instance
 func New(amount int) *Dollar {
 	d := &Dollar{amount: amount}
 	return d
 }
 
+// Times multiply dollar amount and return new instance
 func (d *Dollar) Times(multiplier int) *Dollar {
 	p := &Dollar{amount: d.amount * multiplier}
 	return p
 }
 
+// Equals compare two objects for the value
 func (d *Dollar) Equals(n *Dollar) bool {
 	return d.amount == n.amount
 }
